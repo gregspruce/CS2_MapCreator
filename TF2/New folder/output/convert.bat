66eeebdc0f9d6f5380e8dd3020e7ac3f18a680b0
gregspruce <gregspruce@users.noreply.github.com>
2025-10-09 16:04:33 -0500

    session1 completed_try 2

--- TF2/New folder/output/convert.bat
+++
@@ -1,4 +0,0 @@
-@"./../ImageMagick_7.0.5-7_portable_Q16_x64_stripped/convert.exe" heightmap.tif -set colorspace RGB -channel R -separate +channel -quality 90 %1
-@rem "./../ImageMagick_7.0.5-7_portable_Q16_x64_stripped/convert.exe" heightmap.tif -verbose info:
-@rem "./../ImageMagick_7.0.5-7_portable_Q16_x64_stripped/convert.exe" outfile.png -verbose info:
-@rem @pause
